9ef54e62a7612ffff488c882a363e0b708077ad9
KoptevaAnastasiia <150849726+KoptevaAnastasiia@users.noreply.github.com>
2024-09-12 20:57:12 +0300

    reading the file and checking for valid locations

--- main.cpp
+++ main.cpp
@@ -1,34 +1,115 @@
 #include <iostream>
-#include <iostream>
 using namespace std;
-
 #include <string>
+#include <fstream>
+#include <vector>
 
 class Ticket {
 private:
     string name_ticket;
-    int price_ticket;
+    string price_ticket;
     string date_ticket;
-    int number_ticket;
-    int flight_ticket;
-    int seat_number_ticket;
-    string status_ticket;
+    string number_ticket;
+    string flight_ticket;
+    vector<int> seats;
 
+public:
+    Ticket(string date, string flight, string price, const vector<int>& list_of_seats)
+           : date_ticket(date), flight_ticket(flight), price_ticket(price), seats(list_of_seats) {}
+
+    string getDate() const { return date_ticket; }
+    string getFlight() const { return flight_ticket; }
+    vector<int> getSeats() const { return seats; }
+
+    bool vaild() const { return true; }
 };
 
+
+
 class FileReader {
+public:
+    vector<Ticket> from_file(const string& filename) {
+
+        vector<Ticket> tickets;
+
+        ifstream file(filename);
+        if (!file.is_open()) {
+            cerr << "error :(" << endl;
+            return tickets;
+        }
+
+        string line;
+        while (getline(file, line)) {
+            string date, flight, price, seats_range;
+
+            int index = 0;
 
 
+            while (index < line.size() && line[index] != ' ') {
+                date += line[index];
+                index++;
+            }
+            index++;
+
+
+            while (index < line.size() && line[index] != ' ') {
+                flight += line[index];
+                index++;
+            }
+            index++;
+
+
+
+            while (index < line.size() && line[index] != ' ') {
+                seats_range += line[index];
+                index++;
+            }
+
+            vector<int> seats;
+            int dash = seats_range.find('-');
+            if (dash != string::npos) {
+                int A = stoi(seats_range.substr(0, dash));
+                int B = stoi(seats_range.substr(dash + 1));
+
+
+
+
+                for (int i = A; i <= B; ++i) {
+                    seats.push_back(i);
+                }
+            }
+
+
+
+
+
+
+            tickets.push_back(Ticket(date, flight, price, seats));
+
+
+        }
+
+        file.close();
+        return tickets;
+    }
 
 };
 
 
 class Transport {
 
+private:
+    string flight;
+    string seat;
+    string status_ticket;
+
+
 
 
 };
 
+void status_ticket(const vector<Ticket> &tickets, const string &date, const string &flight);
+void booking(const vector<Ticket> &tickets, const string &date, const string &flight);
 
 int main() {
     int choice;
@@ -42,30 +123,47 @@ int main() {
     cout << "--- Choice ---\n";
     cin >> choice;
 
-
-
+    FileReader fileReader;
+    vector<Ticket> tickets = fileReader.from_file("/home/anastasiia/Documents/oop_01.txt");
 
     switch (choice) {
 
         case 1: {
+            string date;
+            string flight;
 
-            string date; int flight;
             cout << "enter date\n";
             cin >> date;
             cout << "enter flight\n";
             cin >> flight;
 
-
+            status_ticket(tickets, date, flight);
 
             break;
-
-
         }
 
+        case 2: {
+            string date;
+            string flight;
+            int seats;
+            string user_name ;
 
 
-        case 2:
+
+            cout << "enter date\n";
+            cin >> date;
+
+            cout << "enter flight\n";
+            cin >> flight;
+
+            cout << "enter seats\n";
+            cin >> seats;
+
+            cout << "enter user name\n";
+            cin >> user_name;
+
             break;
+        }
 
         case 3:
             break;
@@ -75,8 +173,59 @@ int main() {
 
         default:
             cout << "Invalid Choice\n";
-
     }
+}
+
+
+void status_ticket(const vector<Ticket>& tickets, const string& date, const string& flight) {
+
+
+     for (const auto& ticket : tickets) {
+        if (ticket.getDate() == date && ticket.getFlight() == flight && ticket.vaild()) {
+
+            vector<int> Valid_s = ticket.getSeats();
+
+            if (!Valid_s.empty()) {
+                cout << "List of valid seats : ";
+                for (int seat : Valid_s) {
+                    cout << seat << " , ";
+                }
+                cout << endl;
+
+            }
+        }
+    }
+}
+
+
+
+
+void booking(const vector<Ticket> &tickets, const string &date, const string &flight) {
+
+
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
